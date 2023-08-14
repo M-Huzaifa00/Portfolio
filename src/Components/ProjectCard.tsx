f9c6f514e56016f8d6ed3b48e7ac67b43d492589
Muhammad Huzaifa Abdulahad <hm6154755@gmail.com>
2023-08-14 03:58:11 -0700

    designed project card

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -1,19 +1,29 @@
-import { Card, CardBody, Image} from '@chakra-ui/react'
-interface Props{
-    imgUrl:string ;
-    title:string;
-    detail:string;
-    gitLink:string;
+import { Card, CardBody, Heading, Image, Stack, Text, Divider, CardFooter, Button ,Link} from '@chakra-ui/react'
+import { AiFillGithub } from 'react-icons/ai'
+interface Props {
+    imgUrl: string;
+    title: string;
+    detail: string;
+    gitLink: string;
 
 }
 
-export const ProjectCard = () => {
-  return (
-    <Card>
-        <CardBody>
-            <Image/>
-        </CardBody>
-
-    </Card>
-  )
+export const ProjectCard = ({ imgUrl, title, detail, gitLink }: Props) => {
+    return (
+        <Card>
+            <CardBody>
+                <Image src={imgUrl}/>
+                <Stack mt='6' spacing='3'>
+                    <Heading>{title}</Heading>
+                    <Text>
+                        {detail}
+                    </Text>
+                </Stack>
+            </CardBody>
+            <Divider orientation='horizontal' />
+            <CardFooter>
+                <Button rightIcon={AiFillGithub} ><Link href={gitLink}>Github</Link></Button>
+            </CardFooter>
+        </Card>
+    )
 }
