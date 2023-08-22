21332d765ef0446fdb45768613cb6eb9feb2e4ba
M-Huzaifa00 <121715060+M-Huzaifa00@users.noreply.github.com>
2023-08-22 06:28:19 -0700

    added tyoewriting animation in homepannel </>

--- src/Components/HomePanelBotton.tsx
+++ src/Components/HomePanelBotton.tsx
@@ -1,6 +1,6 @@
 import { Box, VStack, Text } from '@chakra-ui/react';
 import '../App.css'
-
+import { TypeAnimation } from 'react-type-animation';
 
 export const HomePanelBotton = () => {
     return (
@@ -11,7 +11,7 @@ export const HomePanelBotton = () => {
                 >Hello! I'm</Text>
                 <Box style={{ fontFamily: 'Rock Salt' }} fontWeight={'bolder'}>
                     <Text fontSize={{
-                        base:'3xl',
+                        base: '3xl',
                         sm: '2xl',
                         md: '4xl'
                     }} paddingEnd={8}>
@@ -25,13 +25,33 @@ export const HomePanelBotton = () => {
                         Huzaifa
                     </Text>
                 </Box>
-                <Text style={{ fontFamily: 'Montserrat' }} fontWeight={'bold'}
+                <Text style={{ fontFamily: 'Montserrat' }} fontWeight={'bolder'}
+
                     color={'gray.600'}
                 >
-                   {"< cs student />"}
+                    {/* {"< "} */}
+                    <TypeAnimation
+                        sequence={[
+                            '< / >',
+                            2000,
+                            // Same substring at the start will only be typed out once, initially
+                            '< CS STUDENT />',
+                            2000, // wait 1s before replacing "Mice" with "Hamsters"
+                            '< PROGRAMMER />',
+                            2000,
+                            '< DEVELOPER />',
+                            2000
+
+                        ]}
+                        wrapper="span"
+                        speed={30}
+                        repeat={Infinity}
+                    />
+                    {/* {" />"} */}
+
                 </Text>
                 <Text style={{ fontFamily: 'Montserrat' }} fontWeight={'bolder'}
-                    padding={{base:5,md:8,lg:5}} textAlign={'center'}
+                    padding={{ base: 5, md: 8, lg: 5 }} textAlign={'center'}
                     width={{
                         base: '100%',
                         sm: '100%',
