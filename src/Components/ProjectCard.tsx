08d9f97e2775b8709f4f867038acc17f95bd8d16
M-Huzaifa00 <121715060+M-Huzaifa00@users.noreply.github.com>
2023-08-31 13:18:21 -0700

    Live option added

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -1,22 +1,24 @@
 import { Card, CardBody, Heading, Image, Stack, Text, Divider, CardFooter, Button } from '@chakra-ui/react'
 import { AiFillGithub } from 'react-icons/ai'
+import { VscLiveShare } from 'react-icons/vsc'
 import 'aos/dist/aos.css';
 interface Props {
-    id:number;
+    id: number;
     imgUrl: string;
     title: string;
     detail: string;
     gitLink: string;
+    Live?: string;
 
 }
 
-export const ProjectCard = ({ id ,imgUrl, title, detail, gitLink }: Props) => {
+export const ProjectCard = ({ id, imgUrl, title, detail, gitLink, Live }: Props) => {
     const handleClick = (link: string) => {
         window.open(link, '_blank')
     }
     return (
         <Card
-            data-aos={id >0 ? "zoom-in" : undefined}
+            data-aos={id > 0 ? "zoom-in" : undefined}
             padding={{ base: 0, md: 2 }} maxWidth={'300px'}>
             <CardBody>
                 <Image objectFit={'contain'} src={imgUrl} />
@@ -33,13 +35,24 @@ export const ProjectCard = ({ id ,imgUrl, title, detail, gitLink }: Props) => {
                 </Stack>
             </CardBody>
             <Divider />
-            <CardFooter display={'flex'} justifyContent={'center'} >
+            <CardFooter display={'flex'} justifyContent={'center'} gap={2} >
                 <Button
                     fontFamily={'ubuntu'}
                     bgColor={'blackAlpha.200'}
+                    size={'md'}
                     rightIcon={<AiFillGithub />}
                     onClick={() => handleClick(gitLink)}
                 >Github</Button>
+                {
+                    Live && <Button
+                        fontFamily={'ubuntu'}
+                        bgColor={'blackAlpha.200'}
+                        size={'md'}
+                        rightIcon={<VscLiveShare />}
+                        onClick={() => handleClick(Live)}
+                    >Live</Button>
+                }
+
             </CardFooter>
         </Card>
     )
